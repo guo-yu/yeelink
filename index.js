a37c30b831df2f9a33eb99d5547c39dbed11b8ba
Guo Yu <i@guoyu.me>
2013-08-20 18:15:09 +0800

    ship to 0.0.2

---
+++ index.js
@@ -0,0 +1,23 @@
+//                     ___       __  
+//    __  _____  ___  / (_)___  / /__
+//   / / / / _ \/ _ \/ / / __ \/ //_/
+//  / /_/ /  __/  __/ / / / / / ,<   
+//  \__, /\___/\___/_/_/_/ /_/_/|_|  
+// /____/                            
+//
+// @brief: yeeklink api wrapper for node apps
+// @author: [turingou](http://guoyu.me)
+
+var API = require('./api');
+
+var Yeeklink = function(params) {
+    this.key = params.key;
+    this.account = params.account;
+    this.server = 'http://www.yeelink.net/v1.0/';
+    Yeeklink.prototype.device = new API('device',this);
+    Yeeklink.prototype.sensor = new API('sensor',this);
+    Yeeklink.prototype.datapoint = new API('datapoint',this);
+    Yeeklink.prototype.key = new API('key',this);
+};
+
+module.exports = Yeeklink;
